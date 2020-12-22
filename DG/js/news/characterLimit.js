3cf1598b575ed5900d96376d269d5d63d6d1df05
Gabriel Ruiz <51479498+GobsRuiz@users.noreply.github.com>
2020-12-22 07:45:58 -0300

    Change in news from home page

--- DG/js/news/characterLimit.js
+++ DG/js/news/characterLimit.js
@@ -29,12 +29,19 @@ function characterLimit() {
             if(element2.innerText.length >= numberOfCharacters){
                 element2.innerText = element2.innerText.substring(0, numberOfCharacters - 5) + "...";
             }
-        }else if(screen.width < 1024) {
+        }else if(screen.width < 1024  & screen.width > 402) {
             if(element.innerText.length >= numberOfCharacters_responsive){
                 element.innerText = element.innerText.substring(0, numberOfCharacters_responsive - 5) + "...";
             }
             if(element2.innerText.length >= numberOfCharacters_responsive){
                 element2.innerText = element2.innerText.substring(0, numberOfCharacters_responsive - 5) + "...";
+            }
+        }else if(screen.width < 401){
+            if(element.innerText.length >= numberOfCharacters_responsive){
+                element.innerText = element.innerText.substring(0, numberOfCharacters_responsive - 20) + "...";
+            }
+            if(element2.innerText.length >= numberOfCharacters_responsive){
+                element2.innerText = element2.innerText.substring(0, numberOfCharacters_responsive - 20) + "...";
             }
         }
     }
