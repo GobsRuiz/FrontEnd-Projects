d4d7d732723765be7e603a3c924d0c70ebd2ecc5
Gabriel Ruiz <51479498+GobsRuiz@users.noreply.github.com>
2021-03-09 18:46:08 -0300

    Page loaded

---
+++ epicGames2.0/js/pageLoaded.js
@@ -0,0 +1,30 @@
+function functionPageLoaded() {
+    // Logo
+    let logo = document.getElementById("pageLoaded")
+
+
+
+    // Envent listener - load
+    window.addEventListener("load", () => {
+        // Show logo
+        logo.style.opacity = 1;
+
+        // disappear logo
+        setTimeout(() => {
+            logo.style.opacity = 0;
+        }, 3000);
+
+        setTimeout(() => {
+            logo.style.display = "none"
+            // logo - display
+
+            // Body2 - show
+            body2.style.display = "flex"
+
+            setTimeout(() => {
+                body2.style.opacity = "1";
+                body2.style.transform = "scale(1)";
+            }, 20);
+        }, 5000);
+    })
+}functionPageLoaded()
